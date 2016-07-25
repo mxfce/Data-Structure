4d9aa04da2bc472660d4cccf76f746341e4f8984
mxfce <mxfcemxfce@gmail.com>
2016-07-25 21:05:49 +0700

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -3,73 +3,110 @@
 #include <cstring>
 #include <cmath>
 #include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#include <stack>
+#include <queue>
+#include <sstream>
 using namespace std;
-typedef long long ll;
-/////////////////////////////////////////////////////////
-class LinkedList
+class Node
 {
-
-	class Node
-	{
-		private:
-			int data;
-			Node* next = NULL;
-		public:
-			void setData(int n) {data = n;}
-			int getData() { return data; }
-			void setNext(Node* n) { next = n; }
-			Node* getNode() { return next; }
-
-	};
-
-	public:
-		Node* head;
-		LinkedList()
+	public: 
+		Node* nextNode;
+		int val;
+		Node(Node* node)
 		{
-			head = new Node;
+			nextNode = node;
 		}
-		void add(int val)
+		Node(Node* node, int n)
 		{
-			Node* itr = head;
-			while(itr->getNode() != NULL)
-				itr = itr->getNode();
-			Node* n = new Node();
-			n->setData(val);
-			itr->setNext(n);
-		}
-		void printList()
-		{
-			Node* itr = head->getNode();
-			while(itr != NULL)
-			{
-				printf("%d ",itr->getData());
-				itr = itr->getNode();
-			}
-		}
-		void insert(int pos,int val)
-		{
-			int cnt = 0;
-			Node* itr = head->getNode();
-			while(cnt != pos)
-			{
-				itr = itr->getNode();
-				cnt++;
-			}
-			Node* n = new Node;
-			n->setData(val);
-			n->setNext(itr->getNode());
-			itr->setNext(n);
+			nextNode = node;
+			val = n;
 		}
 };
+class LinkedList
+{
+	private:
+		Node* root = new Node(NULL,-1);
+		Node* leaf;
+	public:
+		void add(int n)
+		{
+			Node* newNode = new Node(NULL,n);
+			Node* itr = root;
+			while(itr->nextNode != NULL)
+			{
+				itr = itr->nextNode;
+			}
+			itr->nextNode = newNode;
+		}
+		void insert(int pos, int n)
+		{
+			Node* newNode = new Node(NULL,n);
+			Node* prev = root;
+			Node* curr = root->nextNode;
+			int i = 0;
+			while(curr != NULL)
+			{
+				if(i == pos)
+				{
+					prev->nextNode = newNode;
+					newNode->nextNode = curr;
+					return ;
+				}
+				i++;
+				prev = curr;
+				curr = curr->nextNode;
+				
+			}
+			printf("WARNING! :: Can't Insert, variable pos %d is out of bound\n",pos);
+		}
+		void remove(int val)
+		{
+			Node* curr = root->nextNode;
+			Node* prev = root;
+			while(curr->val != val)
+			{
+				if(curr->nextNode == NULL)
+					return ;
+				prev = curr;
+				curr = curr->nextNode;
+			}
+			prev->nextNode = curr->nextNode;
+		}
+		bool find(int val)
+		{
+			Node* itr = root->nextNode;
+			while(itr != NULL)
+			{
+				if(itr->val == val)
+					return true;
+				itr = itr->nextNode;
+			}
+			return false;
+		}
+		void printAllElements()
+		{
+			Node* itr = root->nextNode;
+			while(itr != NULL)
+			{
+				printf("%d ",itr->val);
+				itr = itr->nextNode;
+			}
+			printf("\n");
+		}
+};
+
 int main()
 {
 	LinkedList* list = new LinkedList;
-	list->add(1);
 	list->add(2);
+	list->add(1);
 	list->add(3);
-	list->printList();
-	printf("\n");
-	list->insert(1,99);
-	list->printList();
-	//
+	list->remove(2);
+	list->remove(3);
+	printf("%d",list->find(3));
+	list->printAllElements();
+	
 }
